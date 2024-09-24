55e37e51df0e940a8b008a02d25918c6d9ff3db4
tran hau huan <huanth03@gmail.com>
2024-09-24 14:50:40 +0700

    add staging to working

---
+++ src/StagingToWorking.java
@@ -0,0 +1,6 @@
+public class StagingToWorking {
+    public static void main(String[] args) {
+        System.out.println("Chuyen tu vung staging sang vung working");
+        System.out.println("Oke");
+    }
+}
