f1c1fa61f6b6655fbdf36195bf9e1fd9457b9f67
tran hau huan <huanth03@gmail.com>
2024-09-24 14:57:01 +0700

    readd repository to working

---
+++ src/RepositoryToWorking.java
@@ -0,0 +1,7 @@
+public class RepositoryToWorking {
+    public static void main(String[] args) {
+        System.out.println("git reset --mixed commit_id");
+        //Chuyển từ trạng thái đã commit về trạng thái trước lúc chạy lệnh git add.
+        //● Tức là từ Repository về lại Working.
+    }
+}
