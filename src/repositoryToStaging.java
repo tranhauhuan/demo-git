a6265a1370ecc40d83f2cec7911d64efdc423e8f
tran hau huan <huanth03@gmail.com>
2024-09-24 14:54:38 +0700

    readd

---
+++ src/repositoryToStaging.java
@@ -0,0 +1,7 @@
+public class repositoryToStaging {
+    public static void main(String[] args) {
+        System.out.println("git reset --soft commit_id");
+        //Chuyển từ trạng thái đã commit về trạng thái trước lúc chạy lệnh git commit.
+        // Tức là từ Repository về lại Staging.
+    }
+}
